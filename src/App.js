4b8bb04bda1de930d1df841792e67ed48ad4f3f4
Donald Whitely <dswhitely1@gmail.com>
2019-10-02 13:26:42 -0400

    Finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data(),
             },
-          }, () => console.log(this.state.currentUser));
+          });
         });
       } else {
         this.setState({ currentUser: userAuth });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './sign-in.styles.scss';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
@@ -19,10 +19,17 @@ class SignIn extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
-  };
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch(error){
+      console.log(error);
+    }
+  }
+  ;
 
   render () {
     return (
